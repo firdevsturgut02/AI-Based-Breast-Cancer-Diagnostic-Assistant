605cb83bfb62751aa2cde647c27027f1b7e8c6c0
firdevsturgut02 <firdevsturgut02@gmail.com>
2025-12-23 22:08:56 +0300

    trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -119,31 +119,35 @@ val_gen = val_datagen.flow_from_dataframe(
 # =====================================================================
 # 6. ATTENTION BLOCK (CBAM)
 # =====================================================================
-def cbam_block(input_tensor, ratio=8):
-    """CBAM: Convolutional Block Attention Module (Channel + Spatial)."""
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def cbam_block(input_tensor, reduction_ratio=8):
     channel = input_tensor.shape[-1]
-    shared_dense_one = Dense(channel // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)
-    shared_dense_two = Dense(channel, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)
 
-    avg_pool = tf.keras.layers.GlobalAveragePooling2D()(input_tensor)
-    avg_pool = tf.keras.layers.Reshape((1, 1, channel))(avg_pool)
-    avg_pool = shared_dense_two(shared_dense_one(avg_pool))
+    # ---- Channel Attention ----
+    avg_pool = layers.GlobalAveragePooling2D()(input_tensor)
+    max_pool = layers.GlobalMaxPooling2D()(input_tensor)
+    
+    avg_dense = layers.Dense(channel // reduction_ratio, activation='relu')(avg_pool)
+    avg_dense = layers.Dense(channel, activation='sigmoid')(avg_dense)
+    
+    max_dense = layers.Dense(channel // reduction_ratio, activation='relu')(max_pool)
+    max_dense = layers.Dense(channel, activation='sigmoid')(max_dense)
 
-    max_pool = tf.keras.layers.GlobalMaxPooling2D()(input_tensor)
-    max_pool = tf.keras.layers.Reshape((1, 1, channel))(max_pool)
-    max_pool = shared_dense_two(shared_dense_one(max_pool))
+    channel_attention = layers.Add()([avg_dense, max_dense])
+    channel_attention = layers.Activation('sigmoid')(channel_attention)
+    channel_refined = layers.Multiply()([input_tensor, layers.Reshape((1,1,channel))(channel_attention)])
 
-    channel_attention = Add()([avg_pool, max_pool])
-    channel_attention = Activation('sigmoid')(channel_attention)
-    channel_refined = Multiply()([input_tensor, channel_attention])
-
-    # Spatial Attention
+    # ---- Spatial Attention ----
     avg_pool = tf.reduce_mean(channel_refined, axis=-1, keepdims=True)
     max_pool = tf.reduce_max(channel_refined, axis=-1, keepdims=True)
-    concat = tf.concat([avg_pool, max_pool], axis=-1)
-    spatial_attention = Conv2D(1, kernel_size=7, padding='same', activation='sigmoid')(concat)
-    refined_output = Multiply()([channel_refined, spatial_attention])
-    return refined_output
+    concat = layers.Concatenate(axis=-1)([avg_pool, max_pool])
+    spatial_attention = layers.Conv2D(1, kernel_size=7, padding='same', activation='sigmoid')(concat)
+
+    refined = layers.Multiply()([channel_refined, spatial_attention])
+    return refined
+
 
 # =====================================================================
 # 7. MODEL DEFINITION (DenseNet121 + CBAM)
